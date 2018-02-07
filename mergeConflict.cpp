dee0de2a6b882a8edcb37370a6b90361cc2947d7
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-02-06 18:38:39 -0900

    Added the original file before a merge conflict.

---
+++ mergeConflict.cpp
@@ -0,0 +1,11 @@
+// mergeConflict.cpp
+// Ryan Stonebraker
+// 2/6/2018
+// An example of a merge conflict resolution using Git.
+
+#include <iostream>
+
+int main () {
+  std::cout << "Pre-Merge Output!" << std::endl;
+  return 0;
+}
