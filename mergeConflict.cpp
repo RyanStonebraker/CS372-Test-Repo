51c1a68a246930494764683a4b00f70f4d805044
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-02-06 18:52:16 -0900

    Merge conflict w/repo

--- mergeConflict.cpp
+++ mergeConflict.cpp
@@ -3,9 +3,20 @@
 // 2/6/2018
 // An example of a merge conflict resolution using Git.
 
-#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main () {
-  std::cout << "Pre-Merge Output!" << std::endl;
+   // spawn a new process
+   pid_t proc = fork();
+
+      // Only perform the write in the child process (when fork returns 0)
+			if (!proc) {
+				write(1, "Merge Conflict!!!\n", 17);
+			} else if (proc < 0) {
+				// Error occured creating new process, an error code was returned
+				_exit(1);
+			}
+      // All good, get out of here
   return 0;
 }
